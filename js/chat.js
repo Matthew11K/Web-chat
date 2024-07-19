b80c189fedc9ce9458c9434f3e1ad3f66af21c1e
Slava Gurow <89160983021@mail.ru>
2024-07-20 01:50:24 +0300

    chat: Добавил модели сообщий input и autput, реализовал отправку сообщений, но сообщения не сохраняются.

--- js/chat.js
+++ js/chat.js
@@ -1,5 +1,22 @@
 function autiResize(textarea) {
     textarea.style.height ='auto';
-    textarea.style.height = textarea.scrollHeight -50 +'px'
+    textarea.style.height = textarea.scrollHeight -50 +'px';
 }
+function SendMessege() {
+    var text = document.getElementById('textMessege');
+    if (text.value.length != 0) {
+        const currentDate = new Date();
+        var newMessege = '<div class="messege-text-right"><p>' + text.value.replace(/\n/g, '<br>') + '</p><p>' + currentDate.toLocaleTimeString().substring(0,5) + '</p></div>';
+        var chat = document.getElementById('main');
+        var lastElement = chat.lastElementChild;
+
+        var newElement = document.createElement('div');
+        newElement.innerHTML = newMessege;
+
+        chat.insertBefore(newElement, lastElement);
+        text.value = ''; 
+    }
+    autiResize(text);
+}
+
 
